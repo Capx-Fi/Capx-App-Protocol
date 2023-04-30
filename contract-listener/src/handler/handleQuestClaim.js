ba74177388eff12ca74c47da02432c328bb0a029
shreyas3336 <shreyasagarwal27@gmail.com>
2023-04-30 17:24:56 +0530

    v0.6.0 - Contract Listener Logic Updated.

--- contract-listener/src/handler/handleQuestClaim.js
+++ contract-listener/src/handler/handleQuestClaim.js
@@ -5,39 +5,6 @@ const { ALCHEMY_ENDPOINTS } = require("../constants/constants.js");
 
 async function handle(env, network, event) {
     try {
-        // Sample Event Object
-        // event = {
-        //     address: '0xDB336B6d6705736C68D73390437766F7c3beadF5',
-        //     blockNumber: 33351964,
-        //     transactionHash: '0xdf78ba4fb62517bb020f64c2249d1dfa6bddeb2ee414d6fe9a05ee2513219a9e',
-        //     transactionIndex: 10,
-        //     blockHash: '0x13cf11e288900b87faf5b87dbededc84e03a28e511701f65e910c7eda8e18eb2',
-        //     logIndex: 15,
-        //     removed: false,
-        //     id: 'log_5d4edfad',
-        //     returnValues: {
-        //     '0': '0x630216e20e31B4ecf0ccb6C57B8980C0dd02a603',
-        //     '1': 'a950643f7e9fabed8a9be1f3befcbb3a_1',
-        //     '2': '0xdea1d0816d88B72F522991B5AB955ca808Dde18D',
-        //     '3': '0xc62E90d28A48479f1b4414f79F557a784E6486f2',
-        //     '4': '100000000000000000',
-        //     questAddress: '0x689efD9E55ba3B16bA4c24533F1a987D9dEd68DF',
-        //     questId: '1e9463eaeeb4137ecf62cbb6567e1ed8_0',
-        //     claimer: '0x8c6404Ce2dAe0Bea0654c5A7D8A6112C06eD6A60',
-        //     rewardToken: '0xB95140C85F8fF72e38E1CE4eAfA4F445ACbCcB6E',
-        //     rewardAmount: '1000000000000000000'
-        //     },
-        //     event: 'CapxQuestRewardClaimed',
-        //     signature: '0x1f316933ef3d09c0e19c6de2b7017efaab425806453d954a28f021c550029e35',
-        //     raw: {
-        //     data: '0x0000000000000000000000000000000000000000000000000000000000000080000000000000000000000000dea1d0816d88b72f522991b5ab955ca808dde18d000000000000000000000000c62e90d28a48479f1b4414f79f557a784e6486f2000000000000000000000000000000000000000000000000016345785d8a0000000000000000000000000000000000000000000000000000000000000000002261393530363433663765396661626564386139626531663362656663626233615f31000000000000000000000000000000000000000000000000000000000000',
-        //     topics: [
-        //         '0x1f316933ef3d09c0e19c6de2b7017efaab425806453d954a28f021c550029e35',
-        //         '0x000000000000000000000000630216e20e31b4ecf0ccb6c57b8980c0dd02a603'
-        //     ]
-        //     }
-        // };
-
         const questAddress = event.returnValues.questAddress;
         const questId = event.returnValues.questId;
         const address = event.returnValues.claimer;
