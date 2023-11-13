35c2d3558cdd5ac86da3b335d46c730dce3399c3
shreyas3336 <shreyasagarwal27@gmail.com>
2023-11-13 22:01:17 +0530

    v3.0.0 - CapxCommunity Quest Contracts.

---
+++ contracts/interfaces/ICapxCommunityQuestForger.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+interface ICapxCommunityQuestForger {
+    error ZeroAddressNotAllowed();
+    error QuestNotActive();
+    error QuestIdUsed();
+
+    event CapxCommunityQuestCreated(
+        address indexed creator,
+        string questId,
+        address rewardToken,
+        uint256 maxParticipants,
+        uint256 totalRewardAmountInWei,
+        uint256 maxRewardAmountInWei
+    );
+
+    event CapxCommunityQuestRewardClaimed(
+        address indexed communityAddress,
+        string questId,
+        address claimReceiver,
+        uint256 timestamp,
+        address rewardToken,
+        uint256 rewardAmount
+    );
+
+    function emitClaim(
+        address communityAddress,
+        string memory questId,
+        address claimReceiver,
+        uint256 timestamp,
+        address rewardToken,
+        uint256 rewardAmount
+    ) external;
+
+    function claimSignerAddress() external view returns(address);
+}
